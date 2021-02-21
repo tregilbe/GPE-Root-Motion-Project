5cf28050ca7cd560005385ac5283e1d500a7da33
Trenton Gilbert <tregilbe@uat.edu>
2021-02-21 05:39:25 -0600

    Renamed Camera script, player aims at mouse

--- Assets/Scripts/AimAtMouse.cs
+++ Assets/Scripts/AimAtMouse.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class AimAtMouse : MonoBehaviour
 {
-    public Transform target;
     public float rotationSpeed = 90f;
 
     private void Update()
     {
-        Quaternion targetRotation = Quaternion.LookRotation(transform.position - transform.position);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        //Detect when there is a mouse click
+        if (Input.GetMouseButton(0))
+        {
+            // Create a plane that the mouse can click on
+            Plane plane = new Plane(Vector3.up, transform.position);
+
+            //Create a ray from the Mouse click position
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // Use UnityEngine.Camera since I have a script named Camera - bad choice - FIXED
+
+            //Initialise the enter variable
+            float enter = 0.0f;
+
+            if (plane.Raycast(ray, out enter))
+            {
+                //Get the point that is clicked
+                Vector3 hitPoint = ray.GetPoint(enter);
+
+                // Rotate towards the mouse
+                Quaternion targetRotation = Quaternion.LookRotation(hitPoint - transform.position);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            }
+        }
     }
 }
--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class CameraMover : MonoBehaviour
 {
     // Adjust the speed for the application.
     public float speed = 1.0f;
