34aeed60be4b128644d41535be09883df769f7a0
Trenton Gilbert <tregilbe@uat.edu>
2021-03-14 14:52:14 -0500

    Gizmo added to spawners

--- Assets/Scripts/TargetSpawner.cs
+++ Assets/Scripts/TargetSpawner.cs
@@ -9,6 +9,8 @@ public class TargetSpawner : MonoBehaviour
 
     public float timeBetween;
     public float nextSpawnTime;
+
+    public Vector3 scale;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,5 +30,14 @@ public class TargetSpawner : MonoBehaviour
                 nextSpawnTime = 0;
             }
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+        Gizmos.color = Color.Lerp(Color.cyan, Color.clear, 0.5f);
+        Gizmos.DrawCube(Vector3.up * scale.y / 2f, scale);
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawRay(Vector3.zero, Vector3.forward * 0.4f);
     }
 }
