4481807183d952b6ecca919a7597a3a93d53eb54
Trenton Gilbert <tregilbe@uat.edu>
2021-03-24 19:58:55 -0500

    Settings Menu

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,10 +6,36 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    public Dropdown resolutionDropdown;
+    public Dropdown qualityDropdown;
+    public Slider masterVolumeSlider;
+    public Toggle fullscreenToggle;
+    public Button applyButton;
+
+
+    /*
+    private void Awake()
+    {
+        // Build resolutions
+        resolutionDropdown.ClearOptions();
+        List resolutions = new List();
+        for (int index = 0; index < Screen.resolutions.Length; index++)
+        {
+            resolutions.Add(string.Format("{0} x {1}", Screen.resolutions[index].width, Screen.resolutions[index].height));
+        }
+        resolutionDropdown.AddOptions(resolutions);
+        // Build quality levels
+        qualityDropdown.ClearOptions();
+        qualityDropdown.AddOptions(QualitySettings.names.ToList());
+    }
+    */
     // Start is called before the first frame update
     void Start()
     {
-
+        masterVolumeSlider.value = PlayerPrefs.GetFloat("Master Volume", masterVolumeSlider.maxValue);
+        fullscreenToggle.isOn = Screen.fullScreen;
+        qualityDropdown.value = QualitySettings.GetQualityLevel();
+        applyButton.interactable = false;
     }
 
     // Update is called once per frame
